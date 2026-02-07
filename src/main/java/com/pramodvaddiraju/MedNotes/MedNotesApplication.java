c0d4695c8d7a64a834eba69ffd2cd63bd23f7e55
pramodvaddi <pramodvaddiraju@gmail.com>
2026-02-07 19:50:49 +0530

    initial commit

---
+++ src/main/java/com/pramodvaddiraju/MedNotes/MedNotesApplication.java
@@ -0,0 +1,13 @@
+package com.pramodvaddiraju.MedNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pramodvaddiraju/MedNotes/MedNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramodvaddiraju.MedNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
